a65d09afe2ee9f4cb9f050093208c1731a058183
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-07-01 15:55:21 +0200

    prelude

--- setup.bash
+++ setup.bash
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
 
-echo "Hello world"
+echo "Alloverse app setup wizard"
+echo "--------------------------"
+
+if [ -f "allo" ]; then
+    echo "Project already initialized; use \`./allo/assist upgrade\` to fetch latest version."
+    exit
+fi
+
+read -p "Create new project in $(pwd)? (y/N) " answer
+if [ "$answer" != "y" ] ; then
+    echo "Exiting..."
+    exit
+fi
+
+echo "ok let's do it"
