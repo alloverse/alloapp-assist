99f3b7efd85592686b641b93ba740173215738fb
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-07-02 14:05:50 +0200

    typo in template app

--- template/lua/main.lua
+++ template/lua/main.lua
@@ -4,16 +4,16 @@ local client = Client(
 )
 local app = App(client)
 
-local controlBoard = ui.Surface(ui.Bounds(0, 0.7, 0,   1, 0.5, 0.1))
+local mainView = ui.Surface(ui.Bounds(0, 0.7, 0,   1, 0.5, 0.1))
 local button = ui.Button(ui.Bounds(0.0, 0.05, 0.0,   0.2, 0.2, 0.1))
 local grabHandle = ui.GrabHandle(ui.Bounds( -0.5, 0.5, 0.3,   0.2, 0.2, 0.2))
-controlBoard:addSubview(button)
-controlBoard:addSubview(grabHandle)
+mainView:addSubview(button)
+mainView:addSubview(grabHandle)
 
 button.onActivated = function()
     print("Hello!")
 end
 
-app.mainView = jukebox
+app.mainView = mainView
 app:connect()
 app:run()
