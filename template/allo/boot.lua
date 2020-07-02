f672f8ea369e5f9a83cc893f35a62e7768553f66
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-07-02 14:54:34 +0200

    assist run WIP

--- template/allo/boot.lua
+++ template/allo/boot.lua
@@ -1,18 +1,27 @@
-scriptPath = arg[0]
-srcDir = string.sub(scriptPath, 1, string.find(scriptPath, "main.lua")-2)
-libDir = srcDir.."/../lib"
+local projHome = arg[1]
+local url = arg[2]
+local srcDir = projHome.."/lua"
+local depsDir = projHome.."/allo/deps"
+local libDir = projHome.."/allo/lib"
 
-package.cpath = string.format("%s;%s/?.so", package.cpath, srcDir)
-package.path = string.format(
-    "%s;%s/?.lua;%s/alloui-lua/lua/?.lua;%s/alloui-lua/lib/cpml/?.lua;%s/alloui-lua/lib/pl/lua/?.lua",
-    package.path,
-    srcDir,
-    libDir,
-    libDir,
-    libDir
-)
+if string.match(package.cpath, "so") then
+    package.cpath = package.cpath..";"..libDir.."/linux64/?.so"
+elseif string.match(package.cpath, "dylib") then
+    package.cpath = package.cpath..";"..libDir.."/osx64/?.dylib"
+elseif string.match(package.cpath, "dll") then
+    package.cpath = package.cpath..";"..libDir.."/win64/?.dll"
+end
 
+package.path = package.path
+    ..";"..srcDir.."/%s/?.lua"
+    ..";"..depsDir.."/alloui-lua/lua/?.lua"
+    ..";"..depsDir.."/alloui-lua/lib/cpml/?.lua"
+    ..";"..depsDir.."/alloui-lua/lib/pl/lua/?.lua"
+
+-- Establish globals
 require("liballonet")
 Client = require("alloui.client")
 ui = require("alloui.ui")
-require("app")
+
+-- start app
+require("main")
