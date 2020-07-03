14d8e4522f17eaad9d80359a4a27106cd0a23880
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-07-03 11:42:25 +0200

    fix build script on mac

--- build.bash
+++ build.bash
@@ -9,5 +9,13 @@ cd ..
 
 cat setup-script.bash > setup.bash
 
-PAYLOAD=$(base64 -w 0 payload.tgz)
-sed -i "s#BASE64ENCODEDTGZ#${PAYLOAD}#g" setup.bash
+plat="$(uname -s)"
+if [[ "$plat" == CYGWIN* ]] || [[ "$plat" == MINGW* ]]
+then
+    PAYLOAD=$(base64 -w 0 payload.tgz)
+    sed -i "s#BASE64ENCODEDTGZ#${PAYLOAD}#g" setup.bash
+else
+    PAYLOAD=$(base64 payload.tgz)
+    sed -i "" "s#BASE64ENCODEDTGZ#${PAYLOAD}#g" setup.bash
+fi
+
