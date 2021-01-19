22cb53dd8772964ace1a2634879e0818375a51e8
Nevyn Bengtsson <nevyn@alloverse.com>
2021-01-19 14:41:29 +0100

    Update main.lua
    Change default location for default app to show up in front of a user instead of slightly behind them, so a new user understands better what's going on

--- template/lua/main.lua
+++ template/lua/main.lua
@@ -4,7 +4,7 @@ local client = Client(
 )
 local app = App(client)
 
-local mainView = ui.Surface(ui.Bounds(0, 0.7, 0,   1, 0.5, 0.1))
+local mainView = ui.Surface(ui.Bounds(0, 1.2, -2,   1, 0.5, 0.1))
 local button = ui.Button(ui.Bounds(0.0, 0.05, 0.0,   0.2, 0.2, 0.1))
 local grabHandle = ui.GrabHandle(ui.Bounds( -0.5, 0.5, 0.3,   0.2, 0.2, 0.2))
 mainView:addSubview(button)
