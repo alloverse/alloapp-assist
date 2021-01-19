beef8191481b9c9e3b82a3f1f3db14ba364efc7f
Nevyn Bengtsson <nevyn@alloverse.com>
2021-01-19 15:16:28 +0100

    Update main.lua
    Quit button, and some inline documentation for newbies

--- template/lua/main.lua
+++ template/lua/main.lua
@@ -1,19 +1,53 @@
+-- a Client is used to connect this app to a Place. arg[2] is the URL of the place to
+-- connect to, which Assist sets up for you.
 local client = Client(
     arg[2], 
     "__PROJNAME__"
 )
+
+-- App manages the Client connection for you, and manages the lifetime of the
+-- your app.
 local app = App(client)
 
-local mainView = ui.Surface(ui.Bounds(0, 1.2, -2,   1, 0.5, 0.1))
-local button = ui.Button(ui.Bounds(0.0, 0.05, 0.0,   0.2, 0.2, 0.1))
-local grabHandle = ui.GrabHandle(ui.Bounds( -0.5, 0.5, 0.3,   0.2, 0.2, 0.2))
-mainView:addSubview(button)
-mainView:addSubview(grabHandle)
+-- mainView is the main UI for your app. Set it up before connecting.
+-- 0, 1.2, -2 means: put the app centered horizontally; 1.2 meters up from the floor; and 2 meters into the room, depth-wise
+-- 1, 0.5, 0.01 means 1 meter wide, 0.5 meters tall, and 1 cm deep.
+-- It's a surface, so the depth should be close to zero.
+local mainView = ui.Surface(ui.Bounds(0, 1.2, -2,   1, 0.5, 0.01))
 
+-- Make it so that the grab button or right mouse button moves lets user move the view.
+-- Instead of making mainView grabbable, you could also create a ui.GrabHandle and add it
+-- as a subview, sort of like a title bar of a desktop window.
+mainView.grabbable = true
+
+-- Create a regular ol' button. Make it 2 dm wide and high, and 1dm deep.
+-- The position refers to the center of the button, so it needs to be moved
+-- 5cm out of the mainView so that the button lies on top of the surface,
+-- instead of embedded inside of it.
+local button = ui.Button(ui.Bounds(0.0, 0.05, 0.05,   0.2, 0.2, 0.1))
+mainView:addSubview(button)
+
+-- When user presses the button, print "hello" to the local terminal.
+-- Here's where you can make the button do more fun stuff :)
 button.onActivated = function()
     print("Hello!")
 end
 
+-- It's nice to provide a way to quit the app, too.
+-- Here's also an alternative syntax for setting the size and position of something.
+local quitButton = ui.Button(ui.Bounds{size=ui.Size(0.12,0.12,0.05)}:move( 0.52,0.25,0.025))
+-- We're still working on support for texture files. As a hack, here's a base64-encoded png.
+quitButton:setDefaultTexture("iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAABUtJREFUeJztm+1TGlcUxp9dcEEQ1wFl7VjT5sXMRE3SWmOssUkax6oBtNM/r39DJ8bXWKNJG3XaRGPbqJlpY0zrS+sC4cUFFYHdfohaQMC9cLc6kOfj7p5nz/l593Dv3pX5dvapgiLVxMoy2JNO4qQ0sbIMAMUJ4KB4oAgBJBYPFBmA1OKBIgKQrnigSABkKh4oAgDZigcKHMBxxQMFDEBN8UCBAlBbPFCAAEiKBwB9LjfZDEm5hGmuRbdIHFNwI4BU7wFoaa4PhaAPBDNfoHYhrmi3YtcMQEkgAKF/EMLQCHThcPqLmON9TCtv8MF398Du7NJNcF+aAODcHlT3D0AvSdBLEuxDI2AjEWKf0r9WUTk+Ac7jhTAwqAkE6gCM6xuoHhxOSpbz+WEfeQAmFiPyqRobByPLhx7CwCB04W2q+VIFYF5+DWHkAZi9vSPnDJsi7GPjwH5B2WT4ZxP20TEw8XjScc7nR3X/fZQEs/QVQlEDYFl6icqHk0BK0okyrq6hcvJx+ua3f4xzu2EfHs04WvRbEoR7A+A8XgpZUwLAz83D+uOUqm5tfrUM68zM0RMM3j3rQ6Ngo9GsHrqdHQj3B2HY+DvXlA+VNwBdeBvlLxaIYiwvFsHPzScdK/H5IAyrb5ZsNApheBTGPCHkBCDx7xw3m+B2dEMuKSHyqHg2C8vSSwBASTAIYXCEuMtHbTbs2auIYlJF5RGICAI83Z1QWDI765NpWBaWYB8Yhm6brLvvVdoguu4Sg08VtSa4W1sLb2cHWZCiwDo1DX0oRBQWtVohupyQDQay+6VR/gASnoft8+fgu9met2U2RXkeossBudRIxS9vAErKdFZqbECwpTlf27SKlVsg9rkQN5uoeWoyFQ40fwbpciNVz1hZGcReF+JlZqq+mi2GfO03EK67QMUrbjLB3edErNxCxS9R2i2HGcDb8SV2z9TmZSOXGiH2OhDleUqJJUvbFyIsC3f3Vzn/VssGA0SXE1GrlXJi/0n7N0IsC5njcgqN2KuwZ7NRTihZ2gKQZVR+/xDG9Y2cwkvX1mGdmqacVLK0A6AoqJx4BNObP/OysSwuoWL2OZ2c0kgbAApge/QDzMuvqdjxs3OwLCxS8UoVfQCKAtuTKZT9/gdVW+vUDMyvyDY91Ig6AOvMTyjbX+XRlm3yMYyra1Q9qQKo+PkpLITvBkjEyDLsY+PgRDc1T2oAKuaeg5//lSyIYbB99mOykFgMwvAoOL+f7F4ZRAVA+S+/gX82Rxznb2uFp6cLW1evEMWxkQiqhkahl/Lfo6QCQDZwAKNilyNBgdbrh4X721oRvlhHFK8PhWBZzL/X5AQgtdRQ/SV4ujoBnU5VfLC5CcGmTxIMGXjv3MbOR2dU5yBdaYS/9brq6zMp7xFwAGP73FmIzh4ox0x7tz69ikDLtTSZsPB0dSJSXX3sPQMt1+Brv6Fqa+04Uf0V2K2pwWafE/HS0rTnpcuN8H/emjFe0evhdnQjasuw+GEY+G59gWBzE410AWgwD9irqoL4TR/i5uQXF6H6S/C1tx0b/24F6Di69tfp4O3sgNRQTzNdbabCUZ6H+LXrEEL4Yh3e3rqpulHGTSa4XY7DkaRwHERHD8IXzlPPVbPF0AEEqbEBb+/cJn5eozwPt/MuYuXl2Ox1YvfDGi3SBJPL/wv8n98IMbKser+hIL8RIt1sIdWpB6C13gM46QROWjl9KJlLszmtIh4BpJ+innYRASi04gECAIVYPKCiBxRq4QfKOgIKvXggC4BiKB7IAKBYigfSACim4oGEJlhshR+IBYq3eAD4FzLNyUTM0XqEAAAAAElFTkSuQmCC")
+quitButton.onActivated = function()
+    app:quit()
+end
+mainView:addSubview(quitButton)
+
+-- Tell the app that mainView is the primary UI for this app
 app.mainView = mainView
+
+-- Connect to the designated remote Place server
 app:connect()
+-- hand over runtime to the app! App will now run forever,
+-- or until the app is shut down (ctrl-C or exit button pressed).
 app:run()
